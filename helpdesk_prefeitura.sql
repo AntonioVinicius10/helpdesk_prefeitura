aca915c4eada4a0d7f316ed57de8cb58a3c41857
Antonio Vinicius <antoniovinixius@gmail.com>
2026-07-25 19:02:57 -0300

    mudança no SQL do Banco

--- helpdesk_prefeitura.sql
+++ helpdesk_prefeitura.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 25-Jul-2026 às 04:18
+-- Tempo de geração: 26-Jul-2026 às 00:00
 -- Versão do servidor: 10.4.11-MariaDB
 -- versão do PHP: 7.4.1
 
@@ -70,18 +70,6 @@ CREATE TABLE `chamados` (
   `fechado_em` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Extraindo dados da tabela `chamados`
---
-
-INSERT INTO `chamados` (`id`, `protocolo`, `titulo`, `descricao`, `status`, `prioridade`, `usuario_id`, `tecnico_id`, `categoria_id`, `criado_em`, `atualizado_em`, `fechado_em`) VALUES
-(2, '', 'Problema de impressora', 'Não imprime as coisa', 'em_andamento', 'media', 7, NULL, 8, '2026-07-25 00:13:45', '2026-07-25 01:55:12', NULL),
-(6, '20260725-3190', 'esta sem internet', 'boa tarde sem internet', 'resolvido', 'media', 7, NULL, 7, '2026-07-25 00:18:46', '2026-07-25 01:32:21', NULL),
-(7, '20260725-4282', 'esta sem internet', 'Tarde boa', 'resolvido', 'media', 7, NULL, 4, '2026-07-25 00:19:31', '2026-07-25 01:29:53', NULL),
-(8, '20260725-5631', 'esta sem internet', 'estou internet desde manhã', 'resolvido', 'media', 9, NULL, 7, '2026-07-25 01:34:41', '2026-07-25 01:55:03', NULL),
-(9, '20260725-8124', 'Problema de impressora', 'A impressora não funciona', 'novo', 'baixa', 9, NULL, 8, '2026-07-25 01:39:02', '2026-07-25 01:45:07', NULL),
-(10, '20260725-1041', 'esta sem internet', 'boa noite', 'novo', 'media', 9, NULL, 2, '2026-07-25 01:46:32', '2026-07-25 01:46:32', NULL);
-
 -- --------------------------------------------------------
 
 --
@@ -95,14 +83,6 @@ CREATE TABLE `chamados_respostas` (
   `mensagem` text NOT NULL,
   `criado_em` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Extraindo dados da tabela `chamados_respostas`
---
-
-INSERT INTO `chamados_respostas` (`id`, `chamado_id`, `usuario_id`, `mensagem`, `criado_em`) VALUES
-(1, 7, 1, 'Já vamos responder', '2026-07-24 22:18:12'),
-(2, 7, 7, 'ok estou esperando', '2026-07-24 22:26:03');
 
 -- --------------------------------------------------------
 
@@ -168,10 +148,7 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `cpf`, `telefone`, `perfil`, `setor_id`, `ativo`, `criado_em`, `atualizado_em`) VALUES
-(1, 'testadmin', 'testadmin@borborema.sp.gov.br', '$2y$10$FFnSpFO6YqO6RSNFFeQzmOoXRq.FV6qvAOy1coGhYhkF1NgJfgXPS', NULL, '(31) 99999-9999', 'admin', 1, 1, '2026-07-23 02:36:02', '2026-07-25 01:05:19'),
-(7, 'test', 'test@gmail.com', '$2y$10$xdw99Nh4NN3VIwPyDpv6puKqwNRzYjw9mdiDqQZ7aHCdyZ/EXXjJa', NULL, '(16) 99614-3988', 'usuario', 1, 1, '2026-07-24 22:47:22', '2026-07-25 01:04:10'),
-(8, 'Tonho', 'antoniovinixius@gmail.com', '$2y$10$ZHM18iKkTT187.C1XQxSuOvQ7vq6wPtxn6wBT.HYf8xpoziQw2BE2', NULL, '(16) 99723-8521', 'tecnico', 1, 1, '2026-07-24 23:03:00', '2026-07-25 01:01:25'),
-(9, 'caio silva', 'caio@gmail.com', '$2y$10$l.qb09eORGp7FJ8tWdP/S.yx4aZh7rJqWqJgh1plge.77wJK9RZfC', NULL, '16997238521', 'usuario', 3, 1, '2026-07-25 01:33:48', '2026-07-25 01:33:48');
+(1, 'testadmin', 'testadmin@borborema.sp.gov.br', '$2y$10$FFnSpFO6YqO6RSNFFeQzmOoXRq.FV6qvAOy1coGhYhkF1NgJfgXPS', NULL, '(31) 99999-9999', 'admin', 1, 1, '2026-07-23 02:36:02', '2026-07-25 01:05:19');
 
 --
 -- Índices para tabelas despejadas
